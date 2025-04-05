3b52350c45d293726cd736503b71575bd6dd3106
Ilya Semenov <ilya@semenov.co>
2025-04-05 13:54:10 +0700

    Replace row_to_json with SELECT *, fix runtime return type

--- packages/orchid/src/worker-utils.ts
+++ packages/orchid/src/worker-utils.ts
@@ -19,8 +19,8 @@ export function makeWorkerUtils<T extends TableClasses>(db: OrchidORM<T>) {
      */
     spec?: TaskSpec,
   ): Promise<DbJob> {
-    const { rows: [{ job }] } = await db.$query<{ job: DbJob }>`
-      SELECT row_to_json(graphile_worker.add_job(
+    const { rows: [job] } = await db.$query<DbJob>`
+      SELECT * FROM graphile_worker.add_job(
         identifier => ${identifier},
         payload => ${payload},
         queue_name => ${spec?.queueName},
@@ -30,7 +30,7 @@ export function makeWorkerUtils<T extends TableClasses>(db: OrchidORM<T>) {
         priority => ${spec?.priority},
         flags => ${spec?.flags},
         job_key_mode => ${spec?.jobKeyMode}
-      )) job
+      )
     `
     return job
   }
@@ -41,13 +41,14 @@ export function makeWorkerUtils<T extends TableClasses>(db: OrchidORM<T>) {
    * Return job data if job was removed.
    */
   async function removeJob(jobKey: string): Promise<DbJob | undefined> {
-    // remove_job() always returns a single row, filled with NULLs if job was not found.
-    const { rows: [{ job }] } = await db.$query<{ job: DbJob }>`
-      SELECT row_to_json(graphile_worker.remove_job(
+    // remove_job() always returns a single row (having all NULLs if not found).
+    const { rows } = await db.$query<DbJob>`
+      SELECT * FROM graphile_worker.remove_job(
         job_key => ${jobKey}
-      )) job
+      )
+      WHERE id IS NOT NULL
     `
-    return job.id ? job : undefined
+    return rows.at(0)
   }
 
   return { addJob, removeJob }
